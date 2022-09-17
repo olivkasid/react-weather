b21b84b41fa3f412c395f4f0c71d63bea93080bd
olivkasid <olga.keit098@gmail.com>
2022-09-17 17:04:30 +0300

    Fixed minutes

--- src/FormatedDate.js
+++ src/FormatedDate.js
@@ -13,10 +13,10 @@ export default function FormatedDate(props) {
   let day = props.date.getDay();
   let hours = props.date.getHours();
   let minutes = props.date.getMinutes();
-  if (minutes < 0) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  if (hours < 0) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   return (
