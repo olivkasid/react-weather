cdf65c06e40c16d2a44445c27a3d9922a637dab9
olivkasid <olga.keit098@gmail.com>
2022-09-25 03:01:38 +0300

    Deleted convertion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,40 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  const [unit, setUnit] = useState("celsius");
-  if (unit === "celsius") {
+
     return (
       <span className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
-          °C|
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
+          °C
         </span>
       </span>
     );
-  } else {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">
-          {Math.round((props.celsius * 9) / 5 + 32)}
-        </span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>
-          |°F
-        </span>
-      </span>
-    );
-  }
 }
